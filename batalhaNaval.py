8d76a811bf736ba82c6ba146f5dff01d290db948
quinti <quintitecnologias@gmail.com>
2025-06-07 03:35:37 -0300

    SOCORRROOOO

--- batalhaNaval.py
+++ batalhaNaval.py
@@ -35,39 +35,49 @@
 import random
 import tkinter as tk
 
-# conversão de letra para numero, coordenadas das linhas
-def converter_letra(letra):
-    if letra == "a" or letra == "1":
+# conversão de caractere para numero, coordenadas das linhas
+def converter_caractere(caractere):
+    if caractere == "a" or caractere == "1":
         return 1
-    elif letra == "b" or letra == "2":
+    elif caractere == "b" or caractere == "2":
         return 2
-    elif letra == "c" or letra == "3":
+    elif caractere == "c" or caractere == "3":
         return 3
-    elif letra == "d" or letra == "4":
+    elif caractere == "d" or caractere == "4":
         return 4
-    elif letra == "e" or letra == "5":
+    elif caractere == "e" or caractere == "5":
         return 5
-    elif letra == "f" or letra == "6":
+    elif caractere == "f" or caractere == "6":
         return 6
-    elif letra == "g" or letra == "7":
+    elif caractere == "g" or caractere == "7":
         return 7
-    elif letra == "h" or letra == "8":
+    elif caractere == "h" or caractere == "8":
         return 8
-    elif letra == "i" or letra == "9":
+    elif caractere == "i" or caractere == "9":
         return 9
-    elif letra == "j" or letra == "10":
+    elif caractere == "j" or caractere == "10":
         return 10
-
-def converter_eixo(eixo):
-    if eixo == "horizontal" or eixo == "x":
-        return "x"
-    elif eixo == "vertical" or eixo == "y":
-        return "y"
     else:
-        print("Digite um valor válido!")
+        print("Digite uma posição válida!")
+        return -1
+    
+def verificar_numero(numero): # para a pessoa nao digitar "A" e sair como "1" na coluna, por exemplo
+    if numero >= 0 and numero <= 10:
+        return numero
+    else:
         return -1
 
-def criar_11x11(): # <-- isso aqui é gambiarra pra fazer o 10x10 com feedback pro usuario
+
+def converter_eixo(eixo):
+    if eixo == "horizontal" or eixo == "x" or eixo == "h":
+        return "x"
+    elif eixo == "vertical" or eixo == "y" or eixo == "v":
+        return "y"
+    else:
+        print("\nDigite um valor válido!\n")
+        return -1
+
+def criar_11x11_front(): # <-- isso aqui é gambiarra pra fazer o 10x10 com feedback pro usuario
 
     return [
     ['-','1','2','3','4','5','6','7','8','9','10'],
@@ -82,20 +92,55 @@ def criar_11x11(): # <-- isso aqui é gambiarra pra fazer o 10x10 com feedback p
     ['I','0','0','0','0','0','0','0','0','0','0'],
     ['J','0','0','0','0','0','0','0','0','0','0'],
     ]
+
+def criar_matriz_back(): # <-- isso aqui é gambiarra pra fazer o 10x10 com feedback pro usuario
+
+    return [
+    # 0 #1 #2 #3 #4 #5 #6 #7 #8 #9 #10#11
+    [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1], #1
+    [-1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,-1], #2
+    [-1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,-1],
+    [-1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,-1],
+    [-1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,-1],
+    [-1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,-1],
+    [-1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,-1],
+    [-1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,-1],
+    [-1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,-1],
+    [-1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,-1],
+    [-1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,-1],
+    [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+    ]
+
     
 def print_matriz(matriz):
 
     print()
-
     for i in range(len(matriz)):
         print(matriz[i])
-
     print()
 
-matrizJogadorFront = criar_11x11() 
-matrizJogadorBack = criar_11x11()
-matrizComputadorFront = criar_11x11()
-matrizComputadorBack = criar_11x11()
+def input_coord():
+
+    linha = -1
+    coluna = -1
+        
+    while linha == -1:
+        linha = input("Digite a LINHA (LETRA) da posição aqui: ").strip().lower()
+        linha = converter_caractere(linha)
+
+    while coluna == -1:
+        coluna = input("Digite a COLUNA (NUMERO) da posição aqui: ").strip().lower()
+        coluna = converter_caractere(coluna)
+        coluna = verificar_numero(coluna)
+
+    
+    return linha, coluna
+
+
+matrizJogadorFront = criar_11x11_front() 
+matrizJogadorBack = criar_matriz_back()
+matrizComputadorFront = criar_11x11_front()
+matrizComputadorBack = criar_matriz_back()
 
 def inserir_na_matriz(matriz, linha, coluna, caractere):
     matriz[linha][coluna] = caractere
@@ -103,6 +148,21 @@ def inserir_na_matriz(matriz, linha, coluna, caractere):
 def verificar_posicao(matriz, linha, coluna):
     if matriz[linha][coluna] != '0':
         print("Local já ocupado!")
+
+def posicionar_barco_hor(matriz,linha,coluna,tamanho):
+        
+        podePosicionar = True
+
+        for i in range(tamanho):
+            if matriz[linha][coluna] > matriz[linha][10] or matriz[linha+i][coluna+i] == -1: # se ele sair do tabuleiro, ou bater num barco(-1), ele seta podePosicionar como falso
+                podePosicionar = False
+
+        if podePosicionar: # preenche as devidas posicoes com o barco
+            for i in range(tamanho):
+                matriz[linha][coluna+i] = -1
+        else:
+            print("O seu barco está saindo do tabuleiro ou colidindo com outro barco! Escolha uma posição válida!")
+            input_coord()
 
 def print_info_barco(nome, tamanho):
     print(f"Você está posicionando o {nome}, ele possui TAMANHO {tamanho}")
@@ -112,6 +172,8 @@ def posicionar_porta():
     barco = "PORTA-AVIÕES"
     tamanho = 5
     eixo = -1
+    linha = -1
+    coluna = -1
 
     print_info_barco(barco,tamanho) 
 
@@ -122,31 +184,31 @@ def posicionar_porta():
         eixo = input("Escolha o eixo: ").lower()
         eixo = converter_eixo(eixo)
 
-    print_matriz(matrizJogadorFront)
+    print_matriz(matrizJogadorBack)
 
-    if eixo == "x":
-        print(f"Você escolheu o eixo HORIZONTAL, o {barco} se estenderá 5 CASAS da ESQUERDA para a DIREITA")
-    elif eixo == "y":
-        print(f"Você escolheu o eixo VERTICAL, o {barco} se estenderá 5 CASAS de CIMA para BAIXO")
-    
-    
-    print("")
-    linha = input("Digite a linha (LETRA) da posição aqui: ").strip().lower()
-    linha = converter_letra(linha)
-    coluna = int(input("Digite a coluna (NUMERO) da posição aqui: ")) # FAZER TRATAMENTO DE ERRO SE ELE ESCOLHER ALGO INVÁLIDO (FORA DO RANGE OU LETRA)
+    linha, coluna = input_coord()
+
+    if eixo == "x": # ARRUMA ESSA BAGAÇA DESSE LOOP QUE TA HORRIVEL NAO TA FUNCINOANDO TO FAZENDO FAZ UMA HORA EU QUERO DORMIR E TEM COISA MAIS IMPORTANTE PRA FAZER
+
+        posicionar_barco_hor(matrizJogadorBack,linha,coluna,tamanho)
+            
+                
+    # elif eixo == "y":
+    #     pass
+                
 
 
 def inicio_jogo():
 
     posicionar_porta()
 
-    print_matriz(matrizJogadorFront)
+    print_matriz(matrizJogadorBack)
     
     print("")
 
-    linha = input("Digite a linha (LETRA) aqui: ").strip().lower()
-    linha = converter_letra(linha)
-    coluna = int(input("Digite a coluna (NUMERO) aqui: ")) # FAZER TRATAMENTO DE ERRO SE ELE ESCOLHER ALGO INVÁLIDO (FORA DO RANGE OU LETRA)
+    linha = input("Digite a linha (caractere) aqui: ").strip().lower()
+    linha = converter_caractere(linha)
+    coluna = int(input("Digite a coluna (NUMERO) aqui: ")) # FAZER TRATAMENTO DE ERRO SE ELE ESCOLHER ALGO INVÁLIDO (FORA DO RANGE OU caractere)
 
     verificar_posicao(matrizJogadorBack, linha, coluna)
     
